b907454d967ab2c4d7fc0162340d3944d5b3a306
mustapha mansouri <143348001+phamusta06@users.noreply.github.com>
2024-07-15 14:11:27 +0100

    Resolved an issue with padding in the chat component

--- src/pages/chat/Chat.tsx
+++ src/pages/chat/Chat.tsx
@@ -6,8 +6,8 @@ import { ChatBuble } from "../../components/chatbuble/ChatBuble"
   const txtArea=useRef(null)
    
   return (
-    <div className="flex justify-center px-2 w-full ">
-        <div className="flex flex-col min-h-screen max-w-[750px] w-full py-3">
+    <div className="flex justify-center w-full ">
+        <div className="flex flex-col min-h-screen max-w-[750px] w-full  ">
           <div className="grow space-y-3  py-3   ">
        <ChatBuble content="fkljge" userType=""/>
        <ChatBuble content="fkljge" userType="fdef"/>
@@ -18,7 +18,7 @@ import { ChatBuble } from "../../components/chatbuble/ChatBuble"
 
           </div>
           <div className="bg-white -full rounded-t-3xl border-[1px] border-gray-700/15 overflow-hidden p-0">
-          <textarea style={{}} ref={txtArea} className="  w-full  px-3 resize-none outline-none overflow-y-hidden max-h-[400px] font-semibold text-lg   " >re</textarea>
+          <textarea style={{}} ref={txtArea} className="  w-full  p-3 resize-none outline-none overflow-y-hidden max-h-[400px] font-semibold text-lg   " >re</textarea>
           <div className="     px-4 text-center">chatbot</div>          </div>
             
          </div>
