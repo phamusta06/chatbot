850930847bfaeb8253fd373f307ecc4a516fd47e
mustapha mansouri <143348001+phamusta06@users.noreply.github.com>
2024-07-15 15:26:07 +0100

    Changed state

--- src/components/cardRecent/CartRecent.tsx
+++ src/components/cardRecent/CartRecent.tsx
@@ -9,12 +9,13 @@ interface CartRecentProps {
 }
 
 const CartRecent: FC<CartRecentProps> = ({ size, content, time ,icon}) => {
+  
     
   
   return (
-    <div className={`flex flex-col justify-between h-${size} border-1 rounded-xl   backdrop-blur-xl shadow-sm p-2 hover `}>
+    <div style={{ height:`${size}rem`}} className={`flex flex-col justify-between  rounded-xl shadow-sm p-4 hover bg-white bg-opacity-90 cursor-pointer hover:bg-white/40 hover:shadow-lg`}>
       <div className={icon?'':'hidden'}><MessagesSquare className="w-4 text-black/70" /></div>
-      <h2 className="font-normal text-base text-black/95">Mdscds</h2>
+      <h2 className="font-semibold text-sm  text-black/90">Mdscds</h2>
       <p className="text-gray-400">a</p>
     </div>
   );
