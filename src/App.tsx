614c1cfe340cf75a9750054fd8af1a524a188bdd
mustapha mansouri <143348001+phamusta06@users.noreply.github.com>
2024-07-15 14:07:32 +0100

    change background

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import './App.css'
 import SideBar from './components/sideBar/SideBar'
 import Chat  from './pages/chat/Chat'
+import { History } from './pages/history/History'
 import { Home } from './pages/home/Home'
+import { Routes,Route  } from 'react-router-dom'
 
 function App() {
  
   return (
-    <div className='flex bg-[#E3F2FD] to-indigo-50 min-h-screen'>
+    <div className='flex bg-gradient-to-br from-gray-200 via-gray-500 to-black/50  min-h-screen'>
       <SideBar/>
-      {/* <Chat/> */}
-      <Home/>
+      <Routes>
+        <Route path='/chat' element={<Chat/>}/>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/history' element={ <History/>}/>
+       
+      </Routes>
 
        
+       
+     
     </div>
   )
 }
