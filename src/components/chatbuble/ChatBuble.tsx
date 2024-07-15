394d4c4196f3b438ab4f0cde75749e5d333ec43a
mustapha mansouri <143348001+phamusta06@users.noreply.github.com>
2024-07-15 15:27:35 +0100

    change style buble

--- src/components/chatbuble/ChatBuble.tsx
+++ src/components/chatbuble/ChatBuble.tsx
@@ -1,28 +1,25 @@
-import logo from '../../assets/logo.png'
-type contentType={
-    content:string,userType:string
- }
-export const ChatBuble = ( props:contentType) => {
-    
+ type contentType = {
+  content: string;
+  userType: string;
+};
+export const ChatBuble = (props: contentType) => {
   return (
-    <div className='px-1'>
-     
-   {     props.userType.length>0 ? (
-            <div className="flex justify-end w-[80%]  ">
-                <img src={logo}  className='w-12 ' />
-                <div className="bg-transparent   rounded-lg
-                    py-2   w-full">
-                    <p className="text-black text-m  ">{props.content}</p>
-                </div>
-            </div>
-        ) : (
-            <div className="flex justify-end">
-                <div className="bg-white rounded-full px-4 py-2 max-w-[80%]">
-                    <p className="text-black text-m">{props.content}</p>
-                </div>
-            </div>
-        )
-     }
+    <div className="px-10 ">
+      {props.userType.length > 0 ? (
+        <div className="flex justify-start    ">
+          
+          <div className="flex items-center bg-white shadow-sm rounded-xl p-2 gap-1  ">
+          <img src={""} className=" bg-black w-6 rounded-full h-6" /> <p className="text-black text-m  ">{props.content}</p>
+          </div>
+        </div>
+      ) : (
+        <div className="flex justify-start">
+           
+          <div className="flex items-center bg-gray-300 shadow-sm rounded-xl p-2 gap-1  ">
+          <img src={""} className=" bg-black w-6 rounded-full h-6" /> <p className="text-black text-m">{props.content}</p>
+          </div>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
