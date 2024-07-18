eeb1e1aee66012e9f220786c7fe020e72d193945
mustapha mansouri <143348001+phamusta06@users.noreply.github.com>
2024-07-18 20:23:20 +0100

    update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,51 @@
-import './App.css'
-import SideBar from './components/sideBar/SideBar'
-import Chat  from './pages/chat/Chat'
-import { History } from './pages/history/History'
-import { Home } from './pages/home/Home'
-import { Routes,Route  } from 'react-router-dom'
+import { useState } from "react";
+import "./App.css";
+import SideBar from "./components/sideBar/SideBar";
+import Chat from "./pages/chat/Chat";
+import { History } from "./pages/history/History";
+import { Home } from "./pages/home/Home";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+import Login from "./pages/auth/Login";
+import Signup from "./pages/auth/Signup";
 
 function App() {
- 
-  return (
-    <div className='flex bg-gradient-to-br from-gray-200 via-gray-500 to-black/50  min-h-screen'>
-      <SideBar/>
-      <Routes>
-        <Route path='/chat' element={<Chat/>}/>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/history' element={ <History/>}/>
-       
-      </Routes>
+  const [token, setToken] = useState(localStorage.getItem("tokenChatBot") || false);
+  const location = useLocation();
 
-       
-       
-     
+  const showSideBar: boolean = ["/login", "/signup"].includes(
+    location.pathname
+  );
+
+  return (
+    <div
+      className={
+        !showSideBar
+          ? `flex min-h-screen`
+          : " "
+      }
+    >
+      {!showSideBar && <SideBar />}
+      <Routes>
+        <Route
+          path="/login"
+          element={!token ? <Login /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/signup"
+          element={!token ? <Signup /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/chat"
+          element={token ? <Chat /> : <Navigate to="/login" />}
+        />
+        <Route path="/" element={token ? <Home /> : <Navigate to="/login" />} />
+        <Route
+          path="/history"
+          element={token ? <History /> : <Navigate to="/login" />}
+        />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
