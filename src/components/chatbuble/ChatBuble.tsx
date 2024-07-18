e0441e16fcfd88300451acea82474552ff66ede5
mustapha mansouri <143348001+phamusta06@users.noreply.github.com>
2024-07-18 20:17:51 +0100

    change direction buble

--- src/components/chatbuble/ChatBuble.tsx
+++ src/components/chatbuble/ChatBuble.tsx
@@ -1,22 +1,23 @@
+ import logo from '../../assets/ai.png'
  type contentType = {
   content: string;
   userType: string;
 };
 export const ChatBuble = (props: contentType) => {
   return (
-    <div className="px-10 ">
+    <div className=" ">
       {props.userType.length > 0 ? (
-        <div className="flex justify-start    ">
+        <div className="flex justify-end   ">
           
-          <div className="flex items-center bg-white shadow-sm rounded-xl p-2 gap-1  ">
-          <img src={""} className=" bg-black w-6 rounded-full h-6" /> <p className="text-black text-m  ">{props.content}</p>
+          <div className="flex items-center bg-white shadow-sm rounded-xl p-2 gap-1 flex-row-reverse  ">
+            <p className="text-black text-m  ">{props.content}</p>
           </div>
         </div>
       ) : (
-        <div className="flex justify-start">
-           
+        <div className="flex justify-start   gap-2">
+           <img src={logo} className="   w-6 rounded-full h-6" />
           <div className="flex items-center bg-gray-300 shadow-sm rounded-xl p-2 gap-1  ">
-          <img src={""} className=" bg-black w-6 rounded-full h-6" /> <p className="text-black text-m">{props.content}</p>
+           <p className="text-black text-m">{props.content}</p>
           </div>
         </div>
       )}
