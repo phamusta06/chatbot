d9bb64faa847de54408ca36c61b6f5ea46765038
mustapha mansouri <143348001+phamusta06@users.noreply.github.com>
2024-07-23 03:47:59 +0100

    add btn logout

--- src/components/sideBar/SideBar.tsx
+++ src/components/sideBar/SideBar.tsx
@@ -1,12 +1,26 @@
 import { Link, NavLink } from "react-router-dom";
 import { IoMdAddCircleOutline } from "react-icons/io";
-
 import logo from "../../assets/ai.png";
 import { BiMessageRoundedDetail } from "react-icons/bi";
+import { LogOut } from "lucide-react";
+import axios from "axios";
+import toast from "react-hot-toast";
+
+const urlApi: string = import.meta.env.VITE_API_URL;
+
 const SideBar = () => {
+  const handleLogout = async () => {
+    try {
+      await axios.get(`${urlApi}/api/v1/logout`);
+      localStorage.removeItem("token");
+      window.location.href = "/";
+    } catch (error: any) {
+      toast.error(error?.message);
+    }
+  };
   return (
-    <div className=" hidden lg:flex group w-10  first-letter:h-[100vh] ">
-      <img src={logo} alt="" className="absolute  w-20 top-2 cursor-pointer" />
+    <div className=" hidden lg:flex group w-10 lg:z-20  first-letter:h-[100vh] ">
+      <img src={logo} alt="" className="fixed  w-14 top-2 cursor-pointer" />
 
       <div
         className={` -translate-x-full fixed top-0 left-0 px-1 group-hover:translate-x-0 h-full w-64 bg-white bg-opacity-85 shadow-lg backdrop-blur-xl overflow-hidden rounded-e-3xl scale-y-[99%] border-[1px] border-gray-700/15 transition-all duration-200`}
@@ -14,7 +28,7 @@ const SideBar = () => {
         <img
           src={logo}
           alt=""
-          className="  w-20 cursor-pointer  object-cover  "
+          className="  w-14 cursor-pointer  object-cover  "
         />
         <Link to={"#"}>
           {" "}
@@ -48,6 +62,11 @@ const SideBar = () => {
               <NavLink to="/history">history</NavLink>
             </li>
           </ul>
+        </div>
+        <div className="absolute bottom-4 left-4">
+          <button onClick={handleLogout}>
+            <LogOut />
+          </button>
         </div>
       </div>
     </div>
