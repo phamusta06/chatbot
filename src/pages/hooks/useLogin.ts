45793a3a84e0569a0402ec0772e9111fdf3355f8
mustapha mansouri <143348001+phamusta06@users.noreply.github.com>
2024-07-30 00:40:11 +0100

    refactor: enhance error handling in useLogin hook

--- src/pages/hooks/useLogin.ts
+++ src/pages/hooks/useLogin.ts
@@ -1,37 +1,39 @@
 import { useState } from "react";
-import toast from 'react-hot-toast';
-import axios from 'axios';
- 
+import toast from "react-hot-toast";
+import axios from "axios";
 
-
-const useLogine = () => {
-  const urlApi:string=import.meta.env.VITE_API_URL
+const useLogin = () => {
+  const urlApi: string = import.meta.env.VITE_API_URL;
   const [loading, setLoading] = useState(false);
-  const login =async ({email,password}:{email:string,password:string}) => {
+
+  const login = async ({ email, password }: { email: string; password: string }) => {
     try {
-      setLoading(true)
-      const res =await axios.post(`${urlApi}/api/v1/login`,{email,password},{withCredentials:true})
-      if(res.data.success){
-         
+      setLoading(true);
+      const res = await axios.post(
+        `${urlApi}/api/v1/login`,
+        { email, password },
+        { withCredentials: true }
+      );
 
-        setLoading(false)
-        localStorage.setItem("tokenChatBot",res.data.success?.token)
-        window.location.href = '/';
+      if (res.data.success) {
+        const token = res.data?.token;
+        if (token) {
+          localStorage.setItem("token", token);
+          window.location.href = "/";
+        } else {
+          toast.error("An unexpected error occurred. Please try again.");
+        }
+      } else {
+        toast.error("Login failed. Please check your credentials and try again.");
       }
-
-
-
-
-    } catch (error:any) {
-      setLoading(false)
-      
-
-     toast.error(error.response?.data?.message||error.message )
-    }finally{
-      setLoading(false)
+    } catch (error: any) {
+      toast.error(error.response?.data?.message || error.message);
+    } finally {
+      setLoading(false);
     }
   };
 
   return { loading, login };
 };
-export default useLogine;
+
+export default useLogin;
