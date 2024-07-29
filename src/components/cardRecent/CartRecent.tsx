534a68c442bc087eb8d37c0f600effbb48372fa4
mustapha mansouri <143348001+phamusta06@users.noreply.github.com>
2024-07-29 21:22:27 +0100

    Added date-fns library for date formatting

--- src/components/cardRecent/CartRecent.tsx
+++ src/components/cardRecent/CartRecent.tsx
@@ -1,22 +1,28 @@
 import { MessagesSquare } from "lucide-react";
 import { FC } from "react";
+import { formatDistanceToNowStrict, parseISO } from 'date-fns';
 
 interface CartRecentProps {
   size: number;
   icon?: boolean;
-  content?: string;
+  content?: any;
   time?: string;
 }
 
-const CartRecent: FC<CartRecentProps> = ({ size, content, time ,icon}) => {
-  
-    
-  
+const CartRecent: FC<CartRecentProps> = ({ size, content, time, icon }) => {
+  const convertToDaysAgo = (dateStr: string) => {
+    const date = parseISO(dateStr);
+    const result = formatDistanceToNowStrict(date, { addSuffix: true });
+    return result;
+  }
+
   return (
-    <div style={{ height:`${size}rem`}} className={`flex flex-col justify-between  rounded-xl shadow-sm p-4 hover bg-white bg-opacity-90 cursor-pointer hover:bg-white/40 hover:shadow-lg`}>
-      <div className={icon?'':'hidden'}><MessagesSquare className="w-4 text-black/70" /></div>
-      <h2 className="font-semibold text-sm  text-black/90">Mdscds</h2>
-      <p className="text-gray-400">a</p>
+    <div style={{ height: `${size}rem` }} className={`flex flex-col justify-between rounded-xl shadow-sm p-4 bg-white bg-opacity-90 cursor-pointer hover:bg-white/90 hover:shadow-lg`}>
+      <div className={icon ? '' : 'hidden'}>
+        <MessagesSquare className="w-4 text-black/70" />
+      </div>
+      <h2 className="font-semibold text-sm text-black/90">{content?.titletMsg?.text}</h2>
+      <p className="text-gray-400">{time ? convertToDaysAgo(time) : ''}</p>
     </div>
   );
 }
