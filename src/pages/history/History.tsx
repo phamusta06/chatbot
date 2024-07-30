62fb4182d631805549407a34f8d343a67c600c65
mustapha mansouri <143348001+phamusta06@users.noreply.github.com>
2024-07-30 03:01:02 +0100

    display previous conversations

--- src/pages/history/History.tsx
+++ src/pages/history/History.tsx
@@ -2,9 +2,23 @@ import { BadgePlus } from "lucide-react";
 import { NavLink } from "react-router-dom";
 import CardRecent from "../../components/cardRecent/CartRecent";
 import { InputSearch } from "./InputSearch";
+import HistoryEmpty from "../../components/historyEmpty/HistoryEmpty";
+import { useContext  } from "react";
+import { UserContext } from "../../context/userContext";
+ 
+ 
+ 
+ 
 
 export const History = () => {
-  return (
+  const  CurrentUserContext=useContext(UserContext)
+  const { user } = CurrentUserContext;
+  
+ 
+      
+    
+  
+   return (
     <div className="relative flex justify-center w-full">
      <div className="flex flex-col gap-6  min-h-screen max-w-[750px] mx-auto w-full  mt-14">
      <button className="absolute top-2 right-2 p-2 bg-slate-900 rounded-xl px-3 text-white hover:bg-slate-700 ">
@@ -14,21 +28,19 @@ export const History = () => {
           <span>Start New Chat</span>
         </span></NavLink> 
       </button>
-       
+      
       <div className="flex flex-col gap-6  min-h-screen max-w-[750px] w-full  ">
 <p className="font-semibold text-center">Your chat history</p>
    <InputSearch/>
-   <div className="flex flex-col gap-2">
-          {
-           
-              [...Array(6)].map((item,index) =>(<CardRecent key={index} size={5} icon={false}/>)
-                  
-                   
-               )
-          
-          }
-
-        </div>
+  {
+    user?.conversations? (
+      user?.conversations?.map((conversation:any, index:number) => (
+         <CardRecent key={index} content={conversation} size={5}  time={conversation?.createdAt}/>
+       )) 
+     ) : (
+       <HistoryEmpty size={30} />
+     )
+  }
       </div>
     
      </div>
