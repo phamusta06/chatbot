4c80c7db3aee2f8c7d7130f05b8202f1c5c63f30
mustapha mansouri <143348001+phamusta06@users.noreply.github.com>
2024-07-30 03:07:19 +0100

    Update texts in previous conversations

--- src/components/sideBar/SideBar.tsx
+++ src/components/sideBar/SideBar.tsx
@@ -1,14 +1,18 @@
-import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { IoMdAddCircleOutline } from "react-icons/io";
 import logo from "../../assets/ai.png";
-import { BiMessageRoundedDetail } from "react-icons/bi";
-import { LogOut } from "lucide-react";
+import { LogOut, MessagesSquare, Text } from "lucide-react";
 import axios from "axios";
 import toast from "react-hot-toast";
+import HistoryEmpty from "../historyEmpty/HistoryEmpty";
+import { useContext } from "react";
+import { UserContext } from "../../context/userContext";
 
 const urlApi: string = import.meta.env.VITE_API_URL;
 
 const SideBar = () => {
+  const  CurrentUserContext=useContext(UserContext)
+  const { user, setUser  } = CurrentUserContext;
   const handleLogout = async () => {
     try {
       await axios.get(`${urlApi}/api/v1/logout`);
@@ -18,20 +22,21 @@ const SideBar = () => {
       toast.error(error?.message);
     }
   };
-  return (
-    <div className=" hidden lg:flex group w-10 lg:z-20  first-letter:h-[100vh] ">
-      <img src={logo} alt="" className="fixed  w-14 top-2 cursor-pointer" />
+  return ( 
+    <div className="   group lg:w-10  z-50    ">
+<img src={logo}   className="hidden lg:block fixed   w-14 top-2 cursor-pointer" />
+<Text className="absolute w-14 top-5 cursor-pointer" />
+
 
       <div
-        className={` -translate-x-full fixed top-0 left-0 px-1 group-hover:translate-x-0 h-full w-64 bg-white bg-opacity-85 shadow-lg backdrop-blur-xl overflow-hidden rounded-e-3xl scale-y-[99%] border-[1px] border-gray-700/15 transition-all duration-200`}
+        className={`-translate-x-full fixed top-0 left-0 px-1 group-hover:translate-x-0 h-full  lg:w-64 bg-white/60 bg-opacity-85 shadow-lg backdrop-blur-xl overflow-hidden rounded-e-3xl scale-y-[99%] border-[1px] border-gray-700/15 transition-all duration-200`}
       >
         <img
           src={logo}
           alt=""
           className="  w-14 cursor-pointer  object-cover  "
         />
-        <Link to={"#"}>
-          {" "}
+        <Link to={"/"}>
           <button className=" flex  items-center gap-1 my-4 text-lg text-start hover:bg-slate-400/30 w-full rounded-md px-1">
             <IoMdAddCircleOutline />
             Start a new chat
@@ -40,27 +45,25 @@ const SideBar = () => {
         <div className="  h-full px-2 space-y-2">
           <h2 className="font-semibold">Recents</h2>
           <ul className="space-y-1">
-            <li>
-              <Link to={"#"}>
-                {" "}
-                <span className=" flex items-center gap-1 font-extralight hover:bg-slate-400/30 rounded-md px-1 ">
+            {user?.conversations?.length != 0 ? (
+             user?.conversations?.map((item:any,index:number) => (
+              <li key={index}>
+              <Link to={`/chat/${item._id}`}>
+                <span className=" flex items-center gap-1 h-5 p-3 overflow-hidden font-extralight hover:bg-slate-400/30 rounded-md px-1 ">
                   <span className="translate-y-[3px]">
-                    <BiMessageRoundedDetail size={13} />
-                  </span>
-                  message for you
-                </span>{" "}
+                  
+                  </span><MessagesSquare className="w-4 text-black/70" />
+                 {
+                  item.titletMsg?.text.substring(0,30)
+
+                 }
+                </span>
               </Link>
             </li>
-
-            <li>
-              <NavLink to="/chat">chat</NavLink>
-            </li>
-            <li>
-              <a href="/">home</a>
-            </li>
-            <li>
-              <NavLink to="/history">history</NavLink>
-            </li>
+             ))
+            ) : (
+              <HistoryEmpty size={60} />
+            )}
           </ul>
         </div>
         <div className="absolute bottom-4 left-4">
@@ -70,6 +73,8 @@ const SideBar = () => {
         </div>
       </div>
     </div>
+ 
+  
   );
 };
 export default SideBar;
