b05531b9d60a810631285b9563b71ce47535c421
mustapha mansouri <143348001+phamusta06@users.noreply.github.com>
2024-08-01 02:24:35 +0100

    add id to iterface CartRecentProps

--- src/components/cardRecent/CartRecent.tsx
+++ src/components/cardRecent/CartRecent.tsx
@@ -1,30 +1,39 @@
 import { MessagesSquare } from "lucide-react";
 import { FC } from "react";
-import { formatDistanceToNowStrict, parseISO } from 'date-fns';
+import { formatDistanceToNowStrict, parseISO } from "date-fns";
+import { useNavigate } from "react-router-dom";
+import useNewMessage from "../../pages/hooks/useNewMessage";
 
 interface CartRecentProps {
   size: number;
   icon?: boolean;
   content?: any;
   time?: string;
+  id: string;
 }
 
-const CartRecent: FC<CartRecentProps> = ({ size, content, time, icon }) => {
+const CartRecent: FC<CartRecentProps> = ({ size, content, time, icon, id }) => {
+  const navigate = useNavigate();
   const convertToDaysAgo = (dateStr: string) => {
     const date = parseISO(dateStr);
     const result = formatDistanceToNowStrict(date, { addSuffix: true });
     return result;
-  }
-
+  };
   return (
-    <div style={{ height: `${size}rem` }} className={`flex flex-col justify-between rounded-xl shadow-sm p-4 bg-white bg-opacity-90 cursor-pointer hover:bg-white/90 hover:shadow-lg`}>
-      <div className={icon ? '' : 'hidden'}>
+    <div
+      onClick={() => navigate(`/chat/${id}`)}
+      style={{ height: `${size}rem` }}
+      className={`flex flex-col justify-between rounded-xl shadow-sm p-4 bg-white bg-opacity-90 cursor-pointer hover:bg-white/90 hover:shadow-lg`}
+    >
+      <div className={icon ? "" : "hidden"}>
         <MessagesSquare className="w-4 text-black/70" />
       </div>
-      <h2 className="font-semibold text-sm text-black/90">{content?.titletMsg?.text}</h2>
-      <p className="text-gray-400">{time ? convertToDaysAgo(time) : ''}</p>
+      <h2 className="font-semibold text-sm text-black/90">
+        {content?.titletMsg?.text}
+      </h2>
+      <p className="text-gray-400">{time ? convertToDaysAgo(time) : ""}</p>
     </div>
   );
-}
+};
 
 export default CartRecent;
