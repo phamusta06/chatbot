e9a31f5e0896ff76956fc0a1c0c09fb3c2a89b52
mustapha mansouri <143348001+phamusta06@users.noreply.github.com>
2024-08-01 01:31:04 +0100

    add useNewMessage hook to manage message state and updates

---
+++ src/pages/hooks/useNewMessage.ts
@@ -0,0 +1,57 @@
+import axios from "axios";
+import { useState } from "react";
+import toast from "react-hot-toast";
+
+ 
+type Message = {
+  conversationId: string;
+  text?: string;
+  userId: string;
+};
+
+ 
+type ApiResponse = {
+  _id: string;
+  conversationId: string;
+  text: string;
+  userId: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
+const urlApi: string = import.meta.env.VITE_API_URL;
+
+const useNewMessage = () => {
+  
+  const [data, setData] = useState<ApiResponse | null>(null);
+ 
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const handleMessage = async ({ conversationId, text, userId }: Message) => {
+    try {
+      setLoading(true);
+
+     
+      const res = await axios.post<ApiResponse>(`${urlApi}/api/v1/new-message`, {
+        conversationId,
+        text,
+        userId,
+      });
+
+   
+      setData(res.data);
+      return res.data;
+
+    } catch (error: any) {
+      
+      toast.error(error?.response?.data?.message || "An error occurred");
+    } finally {
+    
+      setLoading(false);
+    }
+  };
+
+  return { handleMessage, loading, data };
+};
+
+export default useNewMessage;
