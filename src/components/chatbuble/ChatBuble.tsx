f080f5596818577c1d07b2832aab1ec0ada3b458
mustapha mansouri <143348001+phamusta06@users.noreply.github.com>
2024-08-01 02:27:28 +0100

    change direction of bubble element

--- src/components/chatbuble/ChatBuble.tsx
+++ src/components/chatbuble/ChatBuble.tsx
@@ -1,28 +1,28 @@
- import { useContext } from 'react';
+ 
 import logo from '../../assets/ai.png'
-import { UserContext } from '../../context/userContext';
+ 
  
  type contentType = {
   content: string;
   userId: string;
 };
 export const ChatBuble = (props: contentType) => {
-  const  CurrentUserContext=useContext(UserContext)
-  const{user}=CurrentUserContext;
+   
+   
   return (
     <div className=" ">
-      {user.id==="" ? (
+      {props.userId!=="6699b6c51d28f4c2b1a215af" ? (
         <div className="flex justify-end   ">
           
           <div className="flex items-center bg-white shadow-sm rounded-xl p-2 gap-1 flex-row-reverse  ">
-            <p className="text-black text-m  ">{props.content}</p>
+            <p className="text-black text-lg px-3  ">{props.content}</p>
           </div>
         </div>
       ) : (
         <div className="flex justify-start   gap-2">
            <img src={logo} className="   w-6 rounded-full h-6" />
-          <div className="flex items-center bg-gray-300 shadow-sm rounded-xl p-2 gap-1  ">
-           <p className="text-black text-m">{props.content}</p>
+          <div className="flex items-center  shadow-sm rounded-xl p-2 gap-1  ">
+           <p className="text-black text-lg">{props.content}</p>
           </div>
         </div>
       )}
